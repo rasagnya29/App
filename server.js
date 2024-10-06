adc2b4814eb18ac740985d29343861007e6d0241
rasagnya29 <nerella.rasagnya@accenture.com>
2024-10-06 13:19:15 +0000

    crud operations

---
+++ server.js
@@ -0,0 +1,30 @@
+// server.js
+
+const express = require("express");
+// require mongoose 
+const mongoose = require("mongoose")
+
+// initialize the app
+const app = express();
+const PORT = 8080;
+
+
+//mongodb connection
+connectDB()
+.then(()=>console.log("DB is connected successfully"))
+.catch(err => console.log("can not connect",err));
+
+async function connectDB() {
+  await mongoose.connect('mongodb+srv://rasagnyanerella:<db_password>@cluster0.a6fdc.mongodb.net/');
+  //await mongoose.connect('mongodb://152.58.196.57/32://rasagnyanerella:<CAYJ5DcmW5vfIFEq>@cluster0.a6fdc.mongodb.net/');
+
+}
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
