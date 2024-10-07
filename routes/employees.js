be3071e648e13bb9cfe6fa53e06cf84844ec4d7d
rasagnya29 <nerella.rasagnya@accenture.com>
2024-10-07 17:18:59 +0530

    created employees.js

---
+++ routes/employees.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const  employee_Act = require("../controllers/employees"); 
+
+const router = express.Router();
+
+router.get('/', employee_Act.getEmployees);
+router.get('/:id', employee_Act.getspecEmployee);
+router.post('/', employee_Act.createEmployee);
+router.patch('/:id', employee_Act.updateEmployee);
+router.delete('/:id', employee_Act.deleteEmployee);
+
+module.exports=router;
