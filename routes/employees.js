9c6446e44a3e50a16d42d719cb92c15beb87a8cd
rasagnya29 <nerella.rasagnya@accenture.com>
2024-10-07 14:02:02 +0000

    updated

--- routes/employees.js
+++ routes/employees.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get('/', employee_Act.getEmployees);
 router.get('/:id', employee_Act.getspecEmployee);
-router.post('/', employee_Act.createEmployee);
+router.post('/create', employee_Act.createEmployee);
 router.patch('/:id', employee_Act.updateEmployee);
 router.delete('/:id', employee_Act.deleteEmployee);
 
